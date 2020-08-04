a7bad80766953f8f28c7ddc0e76e388f6b4e6dea
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-04 15:52:24 -0300

    refactor: separate routes in centralized file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from  'express'
+import routes from '@/routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.post('/users', (request, response) => {
-  console.log(request.body)
-  response.send('Eu funciono')
-})
+app.use(routes)
 
 app.listen(3333, () => console.log('App is running'))
