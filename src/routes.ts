d1cf505db9f5242e577057311f91e1d9fd76072e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-04 17:31:05 -0300

    refactor: move classes logic to single controller file

--- src/routes.ts
+++ src/routes.ts
@@ -1,66 +1,11 @@
 import express, {Request, Response} from 'express'
-import db from '@/database/connection';
-import {convertHourToMinute} from './utils/convertHourToMinutes';
 
+import ClassesController from '@/controllers/ClassesController'
+
+const classesController = new ClassesController()
 const routes = express.Router()
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
 
-routes.post('/classes', async (request: Request, response: Response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = request.body;
-
-  const trx = await db.transaction()
-
-  try {
-  const insertedUsersIds = await trx('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio
-  })
-
-  const user_id = insertedUsersIds[0]
-
-  const insertedClassesIds = await trx('classes').insert({
-    subject,
-    cost,
-    user_id
-  })
-
-  const class_id = insertedClassesIds[0]
-
-  const classSchedule = schedule.map((scheduleItem:ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinute(scheduleItem.from),
-      to: convertHourToMinute(scheduleItem.to)
-    }
-  })
-
-  await trx('class_schedule').insert(classSchedule)
-
-  await trx.commit()
-
-  return response.status(201).send()
-  } catch (error) {
-    return response.status(400).json({
-      error: 'Internal server error'
-    })
-  }
-
-})
+routes.post('/classes', classesController.create)
 
 export default routes
