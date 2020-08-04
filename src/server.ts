5ca46950541fa042a37912113a59c854324da5d4
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-04 14:35:58 -0300

    chore: add database packages

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hiiiiii')
+import express from  'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/users', (request, response) => {
+  console.log(request.body)
+  response.send('Eu funciono')
+})
+
+app.listen(3333, () => console.log('App is running'))
