7f7f7d88be7633da12b130aa41d8ed1c64480809
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-04 17:13:54 -0300

    feat: add function to convert hours in minutes

---
+++ src/utils/convertHourToMinutes.ts
@@ -0,0 +1,5 @@
+export const convertHourToMinute = (time: string):number => {
+  const [hour, minutes] = time.split(':').map(Number)
+  const timeInMinutes = (hour * 60) + minutes
+  return timeInMinutes
+}
