198b09eaa0986d6d76f50b6d7914392dfffd01bd
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-08-04 19:06:30 -0300

    feat: add create connections

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import express, {Request, Response} from 'express'
 
 import ClassesController from '@/controllers/ClassesController'
+import ConnectionsController from '@/controllers/ConnectionsController'
 
 const classesController = new ClassesController()
+const connectionsController = new ConnectionsController()
 const routes = express.Router()
 
 
 routes.post('/classes', classesController.create)
 routes.get('/classes', classesController.index)
+
+routes.post('/connections', connectionsController.create)
 
 export default routes
